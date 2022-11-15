ebf614fa6be4ee073f5ae866e3135f476da2aee9
Julius <juliusatkinson@gmail.com>
2022-11-15 07:20:36 -0600

    updated map functionality

--- api-project/js/map.js
+++ api-project/js/map.js
@@ -1,8 +1,19 @@
+import {blackBusinesses} from "./config";
+let mymap;
+// console.log (blackBusinesses);
 const initMap=(myJson)=>{
+
+//if a map exist it will be destroyed. Each time we init map we build a brand new map and remove current arrows and builds a new one with new arrows
+    if (mymap && mymap.remove) {
+        mymap.off();
+        mymap.remove();
+      }
+      
     // document.querySelector(".map").innerHTML="map goes here";
     //initialize map:
 // 33.4478679,-112.0550887,17z
-const mymap = L.map('my_map').setView([33.4478679,-112.0550887], 10);
+const center = [myJson.businesses[0].coordinates.latitude, myJson.businesses[0].coordinates.longitude]
+ mymap = L.map('my_map').setView(center, 10);
 
 //initialize tiles (there are many options):
 L.tileLayer.provider('Stamen.TonerLite').addTo(mymap);
@@ -14,42 +25,42 @@ L.tileLayer.provider('Stamen.TonerLite').addTo(mymap);
 // L.tileLayer.provider('Stamen.TonerHybrid').addTo(mymap);
 // L.tileLayer.provider('Stamen.TonerLines').addTo(mymap);
 // L.tileLayer.provider('Stamen.TonerLabels').addTo(mymap);
-
-// const businesses = [
-//     {
-//         title: "Black Theatre Troupe Inc.",
-//         description: "Text text text text text text text text text text text text text text text text text",
-//         lat: 33.4478679,
-//         lng: -112.0550887,
-//         image_url: "https://static.thenounproject.com/png/54604-200.png",
-//         url: "https://www.harkins.com/theatres/scottsdale-101-14/2022-11-03"
-//     },
-//     {
-//         title: "Rita's Italian Ice & Frozen Custard",
-//         description: "Italian ices & frozen custards star at this counter-serve chain with a range of other cold treats.",
-//         lat: 33.6382338,
-//         lng: -111.9804815,
-//         image_url: "https://static.thenounproject.com/png/54604-200.png",
-//         url: "https://www.harkins.com/theatres/scottsdale-101-14/2022-11-03"
-//     },
-//     {
-//         title: "Harkins Theater",
-//         description: "text text text text text text text text text text text text text text text text text",
-//         lat: 33.638214,
-//         lng: -112.0483331,
-//         image_url: "https://static.thenounproject.com/png/54604-200.png",
-//         url: "https://www.harkins.com/theatres/scottsdale-101-14/2022-11-03"
-//     }
-// ];
-
 // https://www.digitalocean.com/community/tutorials/for-loops-for-of-loops-and-for-in-loops-in-javascript#for-of-loop
 for (const business of myJson.businesses) {
     // make the marker:
-    console.log (business);
-    console.log (business.coordinates);
-    console.log ([business.coordinates.latitude, business.coordinates.longitude])
+    // console.log (business);
+    // console.log (business.coordinates);
+    // console.log ([business.coordinates.latitude, business.coordinates.longitude])
+    let myCustomColour = 'gray'
+    if(blackBusinesses.includes(business.id)){
+        myCustomColour= '#eed95b'
+    }
+    console.log (business.id, business.name);
+
+
+const markerHtmlStyles = `
+  background-color: ${myCustomColour};
+  width: 3rem;
+  height: 3rem;
+  display: block;
+  left: -1.5rem;
+  top: -1.5rem;
+  position: relative;
+  border-radius: 3rem 3rem 0;
+  transform: rotate(45deg);
+  border: 1px solid #FFFFFF`
+
+const myIcon = L.divIcon({
+  className: "my-custom-pin",
+  iconAnchor: [0, 24],
+  labelAnchor: [-6, 0],
+  popupAnchor: [0, -36],
+  html: `<span style="${markerHtmlStyles}" />`
+})
+
     const marker = L.marker(
-        [business.coordinates.latitude, business.coordinates.longitude]
+        [business.coordinates.latitude, business.coordinates.longitude],
+        {icon: myIcon}
     ).addTo(mymap);
     // make the popup:
     marker.bindPopup(`
